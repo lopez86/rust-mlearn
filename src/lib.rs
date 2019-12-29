cce546f85ceee114af19d0bb135f8b46e04ca96b
Jeremy Lopez <lopez86@users.noreply.github.com>
2019-12-28 22:56:06 -0500

    Fixing some stuff in the cargo file and a bit more comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
+//! mlearn: Machine Learning Tools in Rust
+//!
+//! This package is meant to provide various machine learning tools
+//! implemented in Rust.
 pub mod regression;
 pub mod linear_regression;
