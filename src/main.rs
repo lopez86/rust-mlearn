779bdb86f66c8cc5e990a5d43c5e817f1276440a
Jeremy Lopez <lopez86@users.noreply.github.com>
2020-01-03 00:18:00 -0500

    Adding a couple unit tests, dev dependencies, will eventually remove backend from cargo file.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use ndarray::array;
 
 use mlearn::model_builder::ModelBuilder;
 use mlearn::linear_regression::{LinearMatrixSolver, ZeroInitializer, LinearRegressor};
-use mlearn::metrics::{mean_absolute_error, mean_squared_error};
 use mlearn::logistic_regression::LogisticRegression;
 use mlearn::regression::Regression;
 use mlearn::classification::{Classification, ClassProbability};
@@ -25,12 +24,8 @@ fn main() {
         coefficients: array![1., 2.],
     };
     println!("Linear regression 2");
-    let preds2 = model2.predict(&my_data);
+    let _preds2 = model2.predict(&my_data);
     println!("Log regression");
-
-    // TODO: Turn into unit tests
-    let myarray1 = array![1., 2., 3.];
-    let myarray2 = array![1., 3., 5.];
 
     let logreg = LogisticRegression::<f32>{
         coefficients: array![1., 2.]
@@ -39,9 +34,4 @@ fn main() {
     println!("{}", pred_proba);
     let preds = logreg.predict(&my_data);
     println!("{}", preds);
-
-    let l1_loss = mean_absolute_error(&myarray1, &myarray2);
-    let l2_loss = mean_squared_error(&myarray1, &myarray2);
-    println!("{}, {}", l1_loss, l2_loss);
-    println!("Should be: 1, 5/3");
 }
--- src/metrics.rs
+++ src/metrics.rs
@@ -36,3 +36,29 @@ where
     let mean = difference.sum() / T::from(difference.len()).unwrap();
     mean
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate netlib_src;
+    use ndarray::array;
+
+    #[test]
+    fn test_mean_squared_error() {
+        let myarray1 = ndarray::array![1., 2., 3.];
+        let myarray2 = ndarray::array![1., 3., 5.];
+        let error = super::mean_squared_error(&myarray1, &myarray2);
+        let epsilon = 1e-6;
+        let expected_result: f64 = 5./3.;
+        assert_eq!( (error - expected_result).abs() < epsilon, true);
+    }
+
+    #[test]
+    fn test_mean_absolute_error() {
+        let myarray1 = ndarray::array![1., 2., 3.];
+        let myarray2 = ndarray::array![1., 3., 5.];
+        let error = super::mean_absolute_error(&myarray1, &myarray2);
+        let epsilon = 1e-6;
+        let expected_result: f64 = 1.;
+        assert_eq!( (error - expected_result).abs() < epsilon, true);
+    }
+}
